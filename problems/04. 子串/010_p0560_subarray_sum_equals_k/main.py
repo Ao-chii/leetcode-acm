d1439e4a0b969da4a9c2c180229c8fed6f50ecaf
Knoon <2543327978@qq.com>
2026-06-02 16:14:52 +0800

    完成p4

--- problems/04. 子串/010_p0560_subarray_sum_equals_k/main.py
+++ problems/04. 子串/010_p0560_subarray_sum_equals_k/main.py
@@ -1,13 +1,22 @@
 import sys
-
-
-def solve(data: str) -> str:
-    return ""
-
+from collections import defaultdict
 
 def main() -> None:
-    data = sys.stdin.read()
-    sys.stdout.write(solve(data))
+    nums = list(map(int, input().split()))
+    k = int(input())
+    ans = 0
+    s = [0] * (len(nums) + 1)
+
+    for i, x in enumerate(nums):
+        s[i + 1] = s[i] + x
+
+    cnt = defaultdict(int)
+    
+    for j in s:
+        ans += cnt[j - k]
+        cnt[j] += 1
+
+    print(ans)  
 
 
 if __name__ == "__main__":
--- problems/04. 子串/011_p0239_sliding_window_maximum/main.py
+++ problems/04. 子串/011_p0239_sliding_window_maximum/main.py
@@ -1,14 +1,22 @@
 import sys
-
-
-def solve(data: str) -> str:
-    return ""
-
+from collections import deque
 
 def main() -> None:
-    data = sys.stdin.read()
-    sys.stdout.write(solve(data))
+    nums = list(map(int, input().split()))
+    k = int(input())
+    q = deque() # 记录窗口元素的下标
+    ans = []
+    for i, num in enumerate(nums):
+        while q and nums[q[-1]] <= num:
+            q.pop()
+        q.append(i)
 
+        if q[0] <= i - k:
+            q.popleft()
 
+        if i >= k - 1:
+            ans.append(nums[q[0]])
+    print(ans)
+    
 if __name__ == "__main__":
     main()
